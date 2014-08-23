e3e6dd48bd7665c6a43e4312ddf59e26141e7566
Mark Kasaboski <mark.tech1@gmail.com>
2014-08-23 01:18:52 -0400

    The main logic for Envariable is now functional. Time for refactoring.

--- src/Envariable/Main.php
+++ src/Envariable/Main.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * @copyright 2014
+ */
 
 namespace Envariable;
 
-class Main
+/**
+ *
+ * @author Mark Kasaboski <markkasaboski@gmail.com>
+ */
+class Envariable
 {
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * ...
+     */
     public function __construct()
     {
         $configFilePath = __DIR__ . '/config/config.php';
@@ -12,21 +27,27 @@ class Main
             throw new \Exception('Configuration file is required.');
         }
 
-        $config = require($configFilePath);
+        $this->config = $this->requireConfigFile($configFilePath);
 
-        $this->defineEnvironment($config);
+        $this->defineEnvironment();
+        $this->defineCustomeEnvironmentConfigs();
     }
 
-    private function defineEnvironment(array $config)
+    /**
+     * Define the environment constant.
+     */
+    private function defineEnvironment()
     {
-        if (defined('STDIN')) {
-            define('ENVIRONMENT', $config['cliDefaultHost']);
+        if (PHP_SAPI === 'cli') {
+            define('ENVIRONMENT', $this->config['cliDefaultHost']);
 
             return;
         }
 
-        foreach ($config['environmentToHostMap'] as $environment => $host) {
-            $this->setEnvironment($environment, $host);
+        foreach ($this->config['environmentToHostMap'] as $environment => $host) {
+            if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] === $host) {
+                define('ENVIRONMENT', $environment);
+            }
         }
 
         if ( ! defined('ENVIRONMENT')) {
@@ -34,10 +55,68 @@ class Main
         }
     }
 
-    private function setEnvironment($environment, $host)
+    /**
+     * Define
+     */
+    private function defineCustomeEnvironmentConfigs()
     {
-        if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] === $host) {
-            define('ENVIRONMENT', $environment);
+        $applicationRootPath             = $this->getApplicationRootPath();
+        $customEnvironmentConfigFilePath = sprintf('%s/.env.%s.php', $applicationRootPath, ENVIRONMENT);
+
+        if ($this->config['customEnvironmentConfigPath'] !== null) {
+            $customEnvironmentConfigPath     = rtrim($this->config['customEnvironmentConfigPath'], '/') . '/';
+            $customEnvironmentConfigFilePath = sprintf('%s/%s.env.%s.php', $applicationRootPath, $customEnvironmentConfigPath, ENVIRONMENT);
         }
+
+        $customEnvironmentConfigMap = $this->requireConfigFile($customEnvironmentConfigFilePath);
+
+        $this->putCustomEnvironmentSettings($customEnvironmentConfigMap);
+    }
+
+    /**
+     * Recurse over the user's custom environment config array and store the
+     * settings within the environment variable store.
+     *
+     * @param array       $configMap
+     * @param string|null $prefix
+     */
+    private function putCustomEnvironmentSettings(array $configMap, $prefix = null)
+    {
+        foreach ($configMap as $key => $value) {
+            if (is_array($value)) {
+                $this->putCustomEnvironmentSettings($value, $key);
+
+                continue;
+            }
+
+            $setting = sprintf('%s_%s=%s', strtoupper($prefix), strtoupper($key), $value);
+
+            putenv($setting);
+        }
+    }
+
+    /**
+     * Retrieve the application root path.
+     *
+     * @return string
+     */
+    private function getApplicationRootPath()
+    {
+        $backtrace = debug_backtrace();
+        $backtrace = end($backtrace);
+
+        return substr($backtrace['file'], 0, strrpos($backtrace['file'], '/'));
+    }
+
+    /**
+     * Load a specified config file.
+     *
+     * @param string $configFilePath
+     *
+     * @return array
+     */
+    private function requireConfigFile($configFilePath)
+    {
+        return require($configFilePath);
     }
 }
