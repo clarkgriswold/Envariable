ece032095f43de9afd58f58b581ffc224fb07c94
Mark Kasaboski <mark.tech1@gmail.com>
2014-08-24 22:49:22 -0400

    Config reverted to old config when I did a git reset HEAD hard and I committed it. Fixed that.

--- src/Envariable/Config/config.php
+++ src/Envariable/Config/config.php
@@ -1,9 +1,34 @@
 <?php
 
 return array(
-    'hostList' => array(
-        'www.adventuredivers.ca',
-        'staging.adventuredivers.ca',
-        'dev.adventuredivers.ca',
+
+    // Array of all hosts your application will use mapped
+    // to their respective environment names.
+    'environmentToHostMap' => array(
+        // Examples:
+        // 'production' => 'example.com',
+        // 'testing'    => 'test.example.com',
     ),
-);
+
+    // The default environment with CLI. You may wish to change
+    // this when testing on your testing, staging or QA servers
+    // (whatever you call it).
+    'cliDefaultHost' => 'production',
+
+    // Path to your custom environment config. It would be
+    // preferrable to keep these outside of your servers public
+    // folder. This likely won't be possible on shared hosting
+    // so set to null for root.
+    //
+    // Envariable figures out the path to the front controller
+    // (index.php) and the path you set in here starts from
+    // there.
+    //
+    // Example:
+    //
+    //     'customEnvironmentConfigPath' => '../' <- Parent directory of application root
+    //
+    // NOTE: Trailing slash is unnecessary, but you can use it if you wish.
+    'customEnvironmentConfigPath' => null,
+
+)
